26c4028b7bd183673b8145a4454a2f896168cbb4
Jeonghan Gam <jeonghan.gam@ascentnet.co.jp>
2018-07-18 15:55:09 +0900

    webserver3 done

--- webserver3.py
+++ webserver3.py
@@ -1,19 +1,28 @@
+import errno
 import os
+import signal
 import socket
-import time
 
 SERVER_ADDRESS = (HOST, PORT) = '', 8888
-REQUEST_QUEUE_SIZE = 5
+REQUEST_QUEUE_SIZE = 1024
+
+
+def grim_reaper(signum, frame):
+    while True:
+        try:
+            pid, status = os.waitpid(
+                -1,          # Wait for any child process
+                 os.WNOHANG  # Do not block and return EWOULDBLOCK error
+            )
+        except OSError:
+            return
+
+        if pid == 0:  # no more zombies
+            return
 
 
 def handle_request(client_connection):
     request = client_connection.recv(1024)
-    print(
-        'Child PID: {pid}. Parent PID {ppid}'.format(
-            pid=os.getpid(),
-            ppid=os.getppid(),
-        )
-    )
     print(request.decode())
     http_response = b"""\
 HTTP/1.1 200 OK
@@ -21,7 +30,6 @@ HTTP/1.1 200 OK
 Hello, World!
 """
     client_connection.sendall(http_response)
-    time.sleep(60)
 
 
 def serve_forever():
@@ -30,16 +38,26 @@ def serve_forever():
     listen_socket.bind(SERVER_ADDRESS)
     listen_socket.listen(REQUEST_QUEUE_SIZE)
     print('Serving HTTP on port {port} ...'.format(port=PORT))
-    print('Parent PID (PPID): {pid}\n'.format(pid=os.getpid()))
+
+    signal.signal(signal.SIGCHLD, grim_reaper)
 
     while True:
-        client_connection, client_address = listen_socket.accept()
+        try:
+            client_connection, client_address = listen_socket.accept()
+        except IOError as e:
+            code, msg = e.args
+            # restart 'accept' if it was interrupted
+            if code == errno.EINTR:
+                continue
+            else:
+                raise
+
         pid = os.fork()
         if pid == 0:  # child
             listen_socket.close()  # close child copy
             handle_request(client_connection)
             client_connection.close()
-            os._exit(0)  # child exits here
+            os._exit(0)
         else:  # parent
             client_connection.close()  # close parent copy and loop over
 
